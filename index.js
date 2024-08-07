b597a9f9cee344bf7bd50e75d3407ce966a3f13e
andresrestrepozapata21 <arz.950203@gmail.com>
2024-08-07 10:54:38 -0500

    chore: Update npm dependencies and cliprogress

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// Importo los modulos que necesito
+// Importar módulos necesarios
 import mysql from 'mysql';
+import cliProgress from 'cli-progress';
+
 // Configuración de MySQL a BBDD testing
 const connection = mysql.createConnection({
-    //host: 'flipo-sql-instance-1.cmgjtomlonmm.eu-west-1.rds.amazonaws.com',
     host: '',
     user: '',
     password: '',
@@ -11,9 +12,7 @@ const connection = mysql.createConnection({
 
 // Promisificar las consultas
 function queryAsync(sql, values) {
-    // Creo una promesa que me realizar la conexion a la base de datos cada que lo necesite
     return new Promise((resolve, reject) => {
-        // conecto y envio la sql que me llegue por parametro
         connection.query(sql, values, (error, results) => {
             if (error) {
                 return reject(error);
@@ -23,9 +22,8 @@ function queryAsync(sql, values) {
     });
 }
 
-// Función para obtener todos los lead de la base de datos
+// Función para obtener todos los leads de la base de datos
 async function getLeadsData() {
-    // SQL para obtener todos los leads
     const queryLead = `
     SELECT BIN_TO_UUID(id) AS id,
            post_code 
@@ -34,79 +32,70 @@ async function getLeadsData() {
     preassigned_optic_process = 0
     LIMIT 500
   `;
-    // Llamo mi metodo para consumar la SQL
     return await queryAsync(queryLead);
 }
 
-// Función principal para insertar en la tabla intermedia entre opticas y leads
+// Función principal para insertar en la tabla intermedia entre ópticas y leads
 async function mainInsertOpticLead(datos) {
-    // Recorro los lead que me llegan por parametro
+    // Inicializar la barra de progreso
+    const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    progressBar.start(datos.length, 0);
+
     for (const lead of datos) {
-        // Capturo las variables que necesito
         const idLead = lead.id;
         const postCode = lead.post_code;
-        // SQl para obtener las coordenada de ese lead
+
         const queryGeographicCache = `
             SELECT * 
             FROM geolocation_cache
             WHERE searched_term = ?
         `;
-        // Llamo y guardo en una variable el resultado de la consulta SQL.
         const results = await queryAsync(queryGeographicCache, [postCode]);
-        // Si este lead tiene resultados
+
         if (results.length > 0) {
-            // Capturo las coordenadas y el pais
             const { lat, lng, id_country } = results[0];
-            // Imprimo en consola para control
             console.log(`Lead ID: ${idLead}, Country: ${id_country}, Post Code: ${postCode}, Latitude: ${lat}, Longitude: ${lng}`);
-            // Segun las coordenadas que tengo, llamo el metodo que me trae las 3 opticas mas cercanas en un radio de 60 km
             const resultNearOptics = await getThreeNearOptics(lat, lng, 60);
-            // Si efectivamente tengo opticas cercanas
+
             if (resultNearOptics.length > 0) {
-                // Ordena por distancia
                 resultNearOptics.sort((a, b) => {
-                    // Calcula la distancia desde las coordenadas dadas hasta cada óptica
                     const a_distance = distance(a.latitude, a.longitude, lat, lng);
                     a.distanceFromCords = a_distance;
                     const b_distance = distance(b.latitude, b.longitude, lat, lng);
                     b.distanceFromCords = b_distance;
-                    // Devuelve la diferencia entre las dos distancias para ordenar el array
                     return a_distance - b_distance;
                 });
-                // Declaro variable para settear el ranking
+
                 let ranking = 1;
-                // Recorro cuales son las opticas para extraerles el ID
                 for (const optic of resultNearOptics) {
-                    // Capturo el id de la optica
                     const { id } = optic;
-                    // SQL para insertar en la tabla intermedia el registro con los ID correspondientes
                     const queryInsertOpticsLead = `
                         INSERT INTO optic_lead(id_lead_ol, id_optic_ol, ranking) VALUES (UUID_TO_BIN('${idLead}'), UUID_TO_BIN('${id}'), ${ranking})
                     `;
-                    // Console log para control
                     console.log(queryInsertOpticsLead);
-                    // Llamo el metodo para consultar la SQL
                     await queryAsync(queryInsertOpticsLead);
-                    // Aumento el ranking segun el orden
                     ranking++;
                 }
             }
         } else {
-            // Cuando no se encuetran georeferencias
             console.log(`No geolocation data found for post code: ${postCode}`);
         }
-        // Actualizo el lead
+
         const queryUpdateLead = `
             UPDATE \`lead\` SET preassigned_optic_process= 1 WHERE \`lead\`.id = UUID_TO_BIN('${idLead}')
         `;
-        // Llamo el metodo para consultar la SQL
         await queryAsync(queryUpdateLead);
+
+        // Actualizar la barra de progreso
+        progressBar.increment();
     }
+
+    // Finalizar la barra de progreso
+    progressBar.stop();
 }
 
-// Función para obtener las 3 opticas cercanas a la ubicacion del lead
+// Función para obtener las 3 ópticas cercanas a la ubicación del lead
 async function getThreeNearOptics(lat, lng, radius) {
-    // SQL para obtener las opticas cercanas en un radio de 60km y maximo 3 opticas
     const queryOptics = `
         SELECT BIN_TO_UUID(optic.id) AS id
         FROM optic
@@ -119,50 +108,31 @@ async function getThreeNearOptics(lat, lng, radius) {
         AND optic.is_active = 1
         LIMIT 3
     `;
-    // Llamo el metodo que me permite consultar la sql
     return await queryAsync(queryOptics);
 }
 
-/*=================================================================================================================================
-                                                    Methods Assistants
-=================================================================================================================================*/
-//Function to calculate distance between two points in km
+// Función para calcular la distancia entre dos puntos en km
 function distance(lat1, lon1, lat2, lon2) {
-    // p es una constante que representa la conversión de grados a radianes (Pi/180)
     const p = 0.017453292519943295;
-
-    // c es un alias para la función coseno de Math
     const c = Math.cos;
-
-    // a es la fórmula del semiverseno, que calcula la distancia entre dos puntos en la superficie de una esfera
     const a =
         0.5 -
         c((lat2 - lat1) * p) / 2 +
         (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
-
-    // La función devuelve la distancia en kilómetros entre los dos puntos
-    // Multiplica el resultado por 12742, que es el doble del radio medio de la Tierra (6371 km)
-    // para convertir la distancia de radianes a kilómetros
     return 12742 * Math.asin(Math.sqrt(a));
 }
 
-/*=================================================================================================================================
-                                                    Methods Main
-=================================================================================================================================*/
 // Función principal para ejecutar el flujo completo
 async function main() {
     try {
-        // Conectar a la base de datos
         connection.connect();
 
-        // Llamo los metod que necesito
         const datos = await getLeadsData();
         await mainInsertOpticLead(datos);
 
     } catch (error) {
         console.error('Error:', error);
     } finally {
-        // Cerrar la conexión a la base de datos
         connection.end();
     }
 }
